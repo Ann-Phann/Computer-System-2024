46331f549a414a7cb905ba221ba9bd8fa3b3460d
An Phan <tqtthuyan71@gmail.com>
2024-09-16 12:34:51 +0930

    update arr min

--- ArrMin/ArrMin.asm
+++ ArrMin/ArrMin.asm
@@ -80,75 +80,97 @@
 
 
 
-// Finds the smallest element in the array of length R2 whose first element is at RAM[R1] and stores the result in R0.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Initialize R0 with a large value
 @32767
 D=A
 @R0
 M=D
 
-// Initialize loop counter i
 @R1
-D=M // Load base address
-@R1
-A=M // Access the first element of the array
-D=M // Store the first element value
-@R0
-M=D // Initialize R0 with the first element (current minimum)
-
-// Set loop counter i
-@i
-M=1
+D=M-1
+@R2
+M=M+D
 
 (LOOP)
+    @R1
+    D=M
     @R2
-    D=M // D = length of the array
-    @i
-    A=M // Get current index
-    D=D-A // D = R2 - i (check if i >= R2)
+    D=D-M
     @END
-    D;JGE // If i >= R2, jump to END
-
-    // Load the next element from the array
+    D;JGT
     @R1
-    D=M // Get base address
-    @i
-    A=D+M // Go to the address of array element at index i
-    D=M // D = array[i]
-
-    // Compare array[i] with the current minimum in R0
+    A=M
+    D=M
+    @NEG
+    D;JLT
     @R0
-    D=D-M // D = array[i] - min
-    @UPDATE_MIN
-    D;JLT // If array[i] < min, jump to UPDATE_MIN
+    D=M
+    @NEG_POS
+    D;JLT
+    @R1
+    A=M
+    D=M
+    @R0
+    D=D-M
+    @PASS
+    D;JGE
 
-    // Increment loop counter i
-    @i
+(REASSIGN_R0)
+    @R1
+    A=M
+    D=M
+    @R0
+    M=D
+
+(PASS)
+    @R1
     M=M+1
     @LOOP
-    0;JMP // Repeat loop
+    0;JMP
 
-(UPDATE_MIN)
-    @R1
-    D=M // Get base address
-    @i
-    A=D+M // Address of array element at index i
-    D=M // D = array[i]
+(POS)
+    @PASS
+    D;JLT
+
+(NEG)
     @R0
-    M=D // Update R0 with the new minimum value
+    D=M
+    @NEG_NEG
+    D;JLT
+    @R1
+    A=M
+    D=M
+    @R0
+    D=D+M
+    @REASSIGN_R0_NEG
+    D;JLT
+    @REASSIGN_R0
+    0;JMP
 
-    // Increment loop counter i
-    @i
-    M=M+1
-    @LOOP
-    0;JMP // Repeat loop
+(NEG_NEG)
+    @R1
+    A=M
+    D=M
+    @R0
+    D=D-M
+    @REASSIGN_R0_NEG
+    D;JLT
+    @PASS
+    0;JMP
+
+(NEG_POS)
+    @PASS
+    0;JMP
+
+(REASSIGN_R0_NEG)
+    @R1
+    A=M
+    D=M
+    @R0
+    M=D
+    @PASS
+    0;JMP
 
 (END)
-    @R0
-    D=M // Load the final minimum value
-    @R0
-    M=D // Store it in R0
     @END
-    0;JMP // Halt execution
+    0;JMP
